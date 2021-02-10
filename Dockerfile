dd1a541a5c475857e6795d75187c7ab7bd14ce2e
Jeremy Carter <jeremy@jeremycarter.ca>
2021-02-10 04:31:05 -0500

    Update github-artifact.js.

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,19 @@
-FROM rust:alpine AS build-env
+FROM rust:slim AS build-env
 
 WORKDIR /root/
 
 COPY src/ src/
 COPY Cargo.toml .
 
-RUN apk --no-cache add openssl-dev musl-dev && \
+RUN apt-get update && apt-get install -y pkg-config libssl-dev && \
   cargo build --release
 
 # ------------------------
 
-FROM alpine:latest
+FROM rust:slim
 
-RUN apk --no-cache add ca-certificates
+# RUN apk --no-cache add ca-certificates openssl curl && \
+#   update-ca-certificates
 
 WORKDIR /root/
 
--- docs/github-artifact.js
+++ docs/github-artifact.js
@@ -31,17 +31,19 @@
 
     console.log('Getting github artifact: https://' + user + ':<redacted>@api.github.com/repos/' + repo_user + '/' + repo_name + '/actions/artifacts');
 
-    // fetch('https://api.github.com/repos/' + repo_user + '/' + repo_name + '/actions/artifacts', {
-    fetch('https://github-artifact-6yr4nbef3q-uc.a.run.app?repo=' + user + ':' + token + '@' + repo_user + '/' + repo_name, {
-      // credentials: 'include',
-      // headers: {
-      //   // 'Accept': 'application/vnd.github.v3+json',
-      //   // 'Authorization': 'Basic ' + user + ':' + token
-      // }
-    }).then(function(res) {
-      return res.json();
-    }).then(function(res) {
-      console.log(res);
-    });
+    window.location.replace('https://github-artifact-6yr4nbef3q-uc.a.run.app?repo=' + user + ':' + token + '@' + repo_user + '/' + repo_name);
+
+    // // fetch('https://api.github.com/repos/' + repo_user + '/' + repo_name + '/actions/artifacts', {
+    // fetch('https://github-artifact-6yr4nbef3q-uc.a.run.app?repo=' + user + ':' + token + '@' + repo_user + '/' + repo_name, {
+    //   // credentials: 'include',
+    //   // headers: {
+    //   //   // 'Accept': 'application/vnd.github.v3+json',
+    //   //   // 'Authorization': 'Basic ' + user + ':' + token
+    //   // }
+    // }).then(function(res) {
+    //   return res.json();
+    // }).then(function(res) {
+    //   console.log(res);
+    // });
   }
 })();
