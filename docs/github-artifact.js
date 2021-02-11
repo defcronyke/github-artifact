3cf451e3ac9a02b5f9b0cdba1c5bf4bd60aff7d4
Jeremy Carter <jeremy@jeremycarter.ca>
2021-02-11 16:37:18 -0500

    Update github-artifact.js.

--- docs/github-artifact.js
+++ docs/github-artifact.js
@@ -6,13 +6,30 @@
     fetch(atob('aHR0cHM6Ly9naXRodWItYXJ0aWZhY3QtNnlyNG5iZWYzcS11Yy5hLnJ1bi5hcHAv') + queryString)
       .then(function (res) {
         if (res.status === 302) {
-          return res.blob();
+          var header = res.headers.get('content-disposition');
+          var contentDispostion = header.split(';');
+          var filenameToken = `filename*=UTF-8''`;
+
+          var filename = 'downloaded.zip';
+          for (var thisValue of contentDispostion) {
+            if (thisValue.trim().indexOf(filenameToken) === 0) {
+              filename = decodeURIComponent(thisValue.trim().replace(filenameToken, ''));
+              break;
+            }
+          }
+
+          return { filename: filename, blob: res.blob() };
         }
       })
       .then(function (res) {
         if (!!res) {
-          var file = window.URL.createObjectURL(res);
-          window.location.assign(file);
+          var url = window.URL.createObjectURL(res.blob);
+          var a = document.createElement('a');
+          a.href = url;
+          a.download = res.filename;
+          document.body.appendChild(a);
+          a.click();
+          a.remove();
 
           if (!!document.referrer) {
             window.location.replace(document.referrer);
