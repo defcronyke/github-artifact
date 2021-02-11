85f4973a0046771999cf08921eb1d26aa1029edf
Jeremy Carter <jeremy@jeremycarter.ca>
2021-02-11 16:15:21 -0500

    Update github-artifact.js.

--- docs/github-artifact.js
+++ docs/github-artifact.js
@@ -4,5 +4,9 @@
 
   if (!!urlParams.get(atob('cmVwbw=='))) {
     window.location.replace(atob('aHR0cHM6Ly9naXRodWItYXJ0aWZhY3QtNnlyNG5iZWYzcS11Yy5hLnJ1bi5hcHAv') + queryString);
+
+    if (!!document.referrer) {
+      window.location.replace(document.referrer);
+    }
   }
 })();
