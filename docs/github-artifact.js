cf613c321dc378eb80cddbdda7f6eaa6d8647da3
Jeremy Carter <jeremy@jeremycarter.ca>
2021-02-11 17:12:26 -0500

    Update github-artifact.js.

--- docs/github-artifact.js
+++ docs/github-artifact.js
@@ -8,43 +8,54 @@
     })
       .then(function (res) {
         console.log('status: ' + res.status);
-        console.log('location header: ' + res.headers.get('location'));
 
-        // if (res.status === 302) {
-        var header = res.headers.get('content-disposition');
-        var filename = 'downloaded.zip';
-
-        if (!!header) {
-          var contentDispostion = header.split(';');
-          var filenameToken = `filename*=UTF-8''`;
-
-          for (var thisValue of contentDispostion) {
-            if (thisValue.trim().indexOf(filenameToken) === 0) {
-              filename = decodeURIComponent(thisValue.trim().replace(filenameToken, ''));
-              break;
-            }
-          }
-        }
-
-        return { filename: filename, blob: res.blob(), location: res.headers.get('location'), res: res };
-        // }
+        return { blob: res.blob(), res: res };
       })
       .then(function (res) {
-        console.log(res);
+        console.log('res: ' + res);
 
         if (!!res) {
-          var url = window.URL.createObjectURL(res.blob);
-          var a = document.createElement('a');
-          a.href = url;
-          a.download = res.filename;
-          document.body.appendChild(a);
-          a.click();
-          a.remove();
+          var fileUrl = res.res.headers.get('url');
 
-          console.log('clicked link: ' + url);
+          if (fileUrl) {
+            fetch(fileUrl)
+              .then(function (res2) {
+                console.log('status2: ' + res2.status);
 
-          if (!!document.referrer) {
-            window.location.replace(document.referrer);
+                var header = res2.headers.get('content-disposition');
+                var filename = 'downloaded.zip';
+
+                if (!!header) {
+                  var contentDispostion = header.split(';');
+                  var filenameToken = `filename*=UTF-8''`;
+
+                  for (var thisValue of contentDispostion) {
+                    if (thisValue.trim().indexOf(filenameToken) === 0) {
+                      filename = decodeURIComponent(thisValue.trim().replace(filenameToken, ''));
+                      break;
+                    }
+                  }
+                }
+
+                return { blob: res2.blob(), res: res2 };
+              })
+              .then(function (res2) {
+                console.log('res2: ' + res2);
+
+                var url = window.URL.createObjectURL(res2.blob);
+                var a = document.createElement('a');
+                a.href = url;
+                a.download = res.filename;
+                document.body.appendChild(a);
+                a.click();
+                a.remove();
+
+                console.log('clicked link: ' + url);
+
+                if (!!document.referrer) {
+                  window.location.replace(document.referrer);
+                }
+              });
           }
         }
       });
